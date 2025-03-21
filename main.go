93caf9cf3046671e479d1b0f3d05db162be2fb01
Hao Nguyen <hao@haonguyen.tech>
2025-03-21 16:39:55 +0700

    feat: read basic file 8 bytes at a time

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	// open the file
+	f, err := os.Open("message.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Read the file 8 bytes at a time
+	b1 := make([]byte, 8)
+
+	for {
+		n, err := f.Read(b1)
+		if err != nil && err != io.EOF {
+			log.Fatalln(err)
+		}
+		if n == 0 {
+			break
+		}
+		fmt.Printf("read: %s\n", string(b1[:n]))
+	}
 }
