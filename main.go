ceec7320cbbf825c1be8a951e592b55cd4d95fa4
Hao Nguyen <hao@haonguyen.tech>
2025-03-23 17:11:56 +0700

    feat: add go routine to read line

--- main.go
+++ main.go
@@ -15,24 +15,38 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	ch := getLinesChannel(f)
+
+	for line := range ch {
+		fmt.Println("read:", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
 	// Read the file 8 bytes at a time
 	buffer := make([]byte, 8)
 
 	currentLineStr := ""
-	for {
-		n, err := f.Read(buffer)
-		if err != nil && err != io.EOF {
-			log.Fatalln(err)
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		for {
+			n, err := f.Read(buffer)
+			if err != nil && err != io.EOF {
+				log.Fatalln(err)
+			}
+			// n == 0 means that we reach the end of the file
+			if n == 0 {
+				break
+			}
+			parts := strings.Split(string(buffer[:n]), "\n")
+			for i := range len(parts) - 1 {
+				ch <- fmt.Sprintf("%s%s", currentLineStr, parts[i])
+				currentLineStr = ""
+			}
+			currentLineStr += parts[len(parts)-1]
 		}
-		// n == 0 means that we reach the end of the file
-		if n == 0 {
-			break
-		}
-		parts := strings.Split(string(buffer[:n]), "\n")
-		for i := range len(parts) - 1 {
-			fmt.Printf("read: %s%s\n", currentLineStr, parts[i])
-			currentLineStr = ""
-		}
-		currentLineStr += parts[len(parts)-1]
-	}
+	}()
+	return ch
 }
