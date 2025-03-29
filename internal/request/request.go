7b2caa485127a1051f2d21e717d55300bd8c74f1
Hao Nguyen <hao@haonguyen.tech>
2025-03-29 17:33:21 +0700

    refactor(request): enhance Request parsing with state management and dynamic buffer handling

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,14 +1,24 @@
 package request
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"strings"
 	"unicode"
 )
 
+type ParseState int
+
+const (
+	stateInitilized ParseState = iota
+	stateDone
+)
+
 type Request struct {
 	RequestLine RequestLine
+	ParseState  ParseState
 }
 
 type RequestLine struct {
@@ -19,61 +29,95 @@ type RequestLine struct {
 
 const crlf = "\r\n"
 
+const bufferSize = 8
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	// Read all bytes from the reader
-	rawBytes, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+	readToIndex := 0
+	r := &Request{
+		ParseState: stateInitilized,
 	}
+	buffer := make([]byte, bufferSize)
+	for r.ParseState != stateDone {
+		if readToIndex >= len(buffer) {
+			newBuf := make([]byte, len(buffer)*2)
+			copy(newBuf, buffer)
+			buffer = newBuf
+		}
+		numBytesRead, err := reader.Read(buffer[readToIndex:])
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				r.ParseState = stateDone
+				break
+			}
+			return nil, err
+		}
+		readToIndex += numBytesRead
 
-	requestLine, err := parseRequestLine(rawBytes)
-	if err != nil {
-		return nil, err
+		numBytesParsed, err := r.parse(buffer[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+		copy(buffer, buffer[numBytesParsed:])
+		readToIndex -= numBytesParsed
 	}
-
-	// Create and return Request object from parsed components
-	return &Request{
-		RequestLine: *requestLine,
-	}, nil
+	return r, nil
 }
 
-func parseRequestLine(data []byte) (*RequestLine, error) {
-	// Convert bytes to string and split on CRLF to get request lines
-	rawRequest := string(data)
-	requestLines := strings.Split(rawRequest, crlf)
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.ParseState {
+	case stateInitilized:
+		requestLine, n, err := parseRequestLine(data)
+		if err != nil {
+			// something actually went wrong
+			return 0, err
+		}
+		if n == 0 {
+			// just need more data
+			return 0, nil
+		}
+		r.RequestLine = *requestLine
+		r.ParseState = stateDone
+		return n, nil
+	case stateDone:
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	default:
+		return 0, fmt.Errorf("unknown state: %q", r.ParseState)
+	}
+}
 
-	// HTTP request must have at least request-line and 2 CRLF
-	if len(requestLines) < 3 {
-		return nil, errors.New("invalid request")
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(data, []byte(crlf))
+	// If it cannot find the \r\n, it means we need more data before we process
+	if idx == -1 {
+		return nil, 0, nil
 	}
 
-	// Parse the request-line which is the first line
-	// Request-line format: Method SP Request-Target SP HTTP-Version CRLF
-	requestLineStr := requestLines[0]
+	requestLineStr := string(data[:idx])
 	requestLineParts := strings.Split(requestLineStr, " ")
 	if len(requestLineParts) != 3 {
-		return nil, errors.New("invalid request line")
+		return nil, 0, errors.New("invalid request line")
 	}
 	methodPart, requestTargetPart, httpVersion := requestLineParts[0], requestLineParts[1], requestLineParts[2]
 
 	for _, c := range methodPart {
 		if !unicode.IsUpper(c) {
-			return nil, errors.New("invalid method: must be uppercase")
+			return nil, 0, errors.New("invalid method: must be uppercase")
 		}
 	}
 
 	if httpVersion != "HTTP/1.1" {
-		return nil, errors.New("unsupport http version, only support HTTP/1.1")
+		return nil, 0, errors.New("unsupport http version, only support HTTP/1.1")
 	}
 
 	versionParts := strings.Split(httpVersion, "/")
 	if len(versionParts) != 2 {
-		return nil, errors.New("invalid http version part")
+		return nil, 0, errors.New("invalid http version part")
 	}
 
+	// Update the Request struct with the Parsed RequestLine
 	return &RequestLine{
 		Method:        methodPart,
 		RequestTarget: requestTargetPart,
 		HttpVersion:   versionParts[1],
-	}, nil
+	}, idx + 2, nil
 }
