2f2bad7444aa177b8615af3b59f0157adff3da95
Hao Nguyen <hao@haonguyen.tech>
2025-04-05 00:29:32 +0700

    feat: add validate field name for header

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 const crlf = "\r\n"
@@ -37,10 +38,31 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	key = strings.TrimSpace(key)
 	value := bytes.TrimSpace(parts[1])
 
+	// Validate field name
+	if err := validateFieldName(key); err != nil {
+		return 0, false, err
+	}
+
+	// Convert key to lowercase
+	key = strings.ToLower(key)
+
 	h.Set(key, string(value))
 	return idx + 2, false, nil
 }
 
 func (h Headers) Set(key, value string) {
 	h[key] = value
+}
+
+func validateFieldName(key string) error {
+	if len(key) < 1 {
+		return fmt.Errorf("field name length must be at least 1")
+	}
+	allowedSpecials := "!#$%&'*+-.^_`|~"
+	for _, c := range key {
+		if !unicode.IsLetter(c) && !unicode.IsDigit(c) && !strings.ContainsRune(allowedSpecials, c) {
+			return fmt.Errorf("field name contains invalid character: %q", c)
+		}
+	}
+	return nil
 }
