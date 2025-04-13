a368bd55d4c3538b144edde39aa5513de237091d
Hao Nguyen <hao@haonguyen.tech>
2025-04-13 17:08:01 +0700

    feat(handler): implement custom request handling with error responses

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"httpfromtcp.haonguyen.tech/internal/request"
+	"httpfromtcp.haonguyen.tech/internal/response"
 	"httpfromtcp.haonguyen.tech/internal/server"
 )
 
 const port = 42069
 
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, testHandler)
 	if err != nil {
 		log.Fatalf("error starting server: %v\n", err)
 	}
@@ -28,4 +32,30 @@ func main() {
 	signal.Notify(sigChn, syscall.SIGINT, syscall.SIGTERM)
 	<-sigChn
 	log.Println("Server gracefully shutdown")
+}
+
+func testHandler(w io.Writer, req *request.Request) *server.HandlerError {
+	switch req.RequestLine.RequestTarget {
+	case "/yourproblem":
+		return &server.HandlerError{
+			Message:    "Your problem is not my problem\n",
+			StatusCode: response.StatusBadRequest,
+		}
+	case "/myproblem":
+		return &server.HandlerError{
+			Message:    "Woopsie, my bad\n",
+			StatusCode: response.StatusServerInternalError,
+		}
+	default:
+		body := "All good, frfr\n"
+		_, err := w.Write([]byte(body))
+		if err != nil {
+			return &server.HandlerError{
+				StatusCode: response.StatusServerInternalError,
+				Message:    fmt.Sprintf("error writing body for valid request: %v\n", err),
+			}
+		}
+	}
+
+	return nil
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,20 +1,23 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net"
 	"sync/atomic"
 
+	"httpfromtcp.haonguyen.tech/internal/request"
 	"httpfromtcp.haonguyen.tech/internal/response"
 )
 
 type Server struct {
 	listener net.Listener
 	isClosed atomic.Bool
+	handler  Handler
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return nil, err
@@ -22,6 +25,7 @@ func Serve(port int) (*Server, error) {
 
 	s := &Server{
 		listener: listener,
+		handler:  handler,
 	}
 
 	go s.listen()
@@ -55,13 +59,39 @@ func (s *Server) handle(conn net.Conn) {
 		}
 	}()
 
-	err := response.WriteStatusLine(conn, response.StatusOK)
+	r, err := request.RequestFromReader(conn)
 	if err != nil {
-		log.Printf("error handle connection when writing status line: %v\n", err)
+		log.Printf("error: parsing request: %v\n", err)
+		hErr := &HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message:    err.Error(),
+		}
+		hErr.Write(conn)
+		return
+	}
+	buf := bytes.NewBuffer([]byte{})
+	handlerErr := s.handler(buf, r)
+	if handlerErr != nil {
+		handlerErr.Write(conn)
+		return
 	}
 
-	err = response.WriteHeaders(conn, response.GetDefaultHeaders(0))
+	b := buf.Bytes()
+
+	err = response.WriteStatusLine(conn, response.StatusOK)
+	if err != nil {
+		log.Printf("error handle connection when writing status line: %v\n", err)
+		return
+	}
+	headers := response.GetDefaultHeaders(len(b))
+	err = response.WriteHeaders(conn, headers)
 	if err != nil {
 		log.Printf("error handle connection when writing headers: %v\n", err)
+		return
+	}
+	_, err = conn.Write(b)
+	if err != nil {
+		log.Printf("error writing body: %v\n", err)
+		return
 	}
 }
